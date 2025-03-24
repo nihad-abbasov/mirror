afb985075fa4049832feb2de928b96d65e6cd058
Nihad Abbasov <nihad.abbasov.work@gmail.com>
2025-03-24 13:12:37 +0400

    contact page form been done

--- src/views/ContactView.tsx
+++ src/views/ContactView.tsx
@@ -1,8 +1,37 @@
 "use client";
 
+import { InputTextareaField } from "@/components/form/InputTextareaField";
+import { InputTextField } from "@/components/form/InputTextField";
+import { CFormProvider } from "@/components/form/CFormProvider";
 import { MotionFadeIn } from "@/components/MotionFadeIn";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { useEffect } from "react";
+
+const schema = yup.object().shape({
+  fullname: yup.string().required("Bu xana vacibdir"),
+  email: yup.string().required("Bu xana vacibdir"),
+  message: yup.string().required("Bu xana vacibdir"),
+});
+
+type FormValues = yup.InferType<typeof schema>;
 
 export const ContactView = () => {
+  const methods = useForm<FormValues>({
+    resolver: yupResolver(schema),
+  });
+
+  const {
+    formState: { isSubmitting },
+  } = methods;
+
+  const handleSubmit = async (formData: FormValues) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    console.log("Form submitted:", formData);
+    await methods.reset();
+  };
+
   return (
     <section className="w-full">
       {/* Page Heading */}
@@ -44,59 +73,37 @@ export const ContactView = () => {
 
           {/* Contact Form */}
           <MotionFadeIn delay={0.2}>
-            <form className="space-y-6">
-              <div>
-                <label
-                  className="block mb-1 text-sm text-gray-700"
-                  htmlFor="name"
-                >
-                  Adınız
-                </label>
-                <input
-                  type="text"
-                  id="name"
-                  name="name"
-                  className="w-full border border-gray-300 rounded-md px-4 py-2 outline-none focus:ring-1 focus:ring-black focus:border-black"
-                  placeholder="Ad Soyad"
-                />
-              </div>
-              <div>
-                <label
-                  className="block mb-1 text-sm text-gray-700"
-                  htmlFor="email"
-                >
-                  Email
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  className="w-full border border-gray-300 rounded-md px-4 py-2 outline-none focus:ring-1 focus:ring-black focus:border-black"
-                  placeholder="example@mail.com"
-                />
-              </div>
-              <div>
-                <label
-                  className="block mb-1 text-sm text-gray-700"
-                  htmlFor="message"
-                >
-                  Mesajınız
-                </label>
-                <textarea
-                  id="message"
-                  name="message"
-                  rows={5}
-                  className="w-full border border-gray-300 rounded-md px-4 py-2 outline-none focus:ring-1 focus:ring-black focus:border-black resize-none"
-                  placeholder="Mesajınızı buraya yazın..."
-                />
-              </div>
-              <button
-                type="button"
-                className="inline-block px-6 py-2 border border-black text-black hover:bg-black hover:text-white transition-colors duration-300 cursor-pointer"
-              >
-                Göndər
-              </button>
-            </form>
+            <CFormProvider
+              methods={methods}
+              onSubmit={handleSubmit}
+              className="space-y-4"
+            >
+              <InputTextField
+                name="fullname"
+                label="Ad Soyad"
+                placeholder="Abbas Abbasov"
+                className="w-full px-4 py-2 border border-gray-300 rounded-md text-sm outline-none "
+              />
+
+              <InputTextField
+                name="email"
+                label="Email"
+                placeholder="mirror@gmail.com"
+                className="w-full px-4 py-2 border border-gray-300 rounded-md text-sm outline-none "
+              />
+
+              <InputTextareaField
+                name="message"
+                label="Mesajınız"
+                placeholder="İstəklərinizi burada qeyd edin"
+              />
+              <input
+                type="submit"
+                disabled={isSubmitting}
+                value={isSubmitting ? "Göndərilir" : "Göndər"}
+                className="inline-block px-6 py-2 border border-black text-black hover:bg-black hover:text-white transition-colors duration-300 cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed"
+              />
+            </CFormProvider>
           </MotionFadeIn>
         </div>
       </article>
